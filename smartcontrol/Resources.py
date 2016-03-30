f6c9e8e7383ef08f8f9679c8959b7ab0c6ecaca4
Gabriel Constenla <grconstenla@gmail.com>
2016-03-30 18:30:11 -0300

    Fix resources directory

--- smartcontrol/Resources.py
+++ smartcontrol/Resources.py
@@ -12,8 +12,9 @@ class Resources(object):
         base = None
         if "python" in os.path.basename(sys.executable):
             base = os.path.abspath(os.path.dirname(sys.argv[0]))
-        elif sys.platform == "win32":
+            if not sys.platform == "win32":
+                base = os.path.join(base, "..", "share", "smartcontrol")
+        elif hasattr(sys, "frozen"):
             base = os.path.abspath(os.path.dirname(sys.executable))
-        else:
-            base = os.path.join(os.path.abspath(os.path.dirname(sys.executable)), "..", "share", "smartcontrol")
+
         return os.path.join(base, "resources")
